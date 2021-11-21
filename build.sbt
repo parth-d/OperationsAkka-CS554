12e666b0a6db4cdf7b4d3808f0a67a5c2694ecac
my <my@MacBook-Air-2.local>
2021-11-21 16:24:58 -0600

    Added logging

--- build.sbt
+++ build.sbt
@@ -4,9 +4,15 @@ version := "0.1"
 
 scalaVersion := "2.13.7"
 
-val AkkaVersion = "2.6.17"
+val AkkaVersion = "2.6.11"
 val ConfigVersion = "1.4.1"
+val LogbackVer = "1.2.3"
+val ScalaTestVer = "3.2.2"
 
 libraryDependencies += "com.typesafe.akka" %% "akka-actor" % AkkaVersion
+libraryDependencies += "com.typesafe.akka" %% "akka-slf4j" % AkkaVersion
+libraryDependencies += "ch.qos.logback" % "logback-classic" % LogbackVer
 libraryDependencies += "org.scala-lang.modules" %% "scala-parallel-collections" % "1.0.4"
 libraryDependencies += "com.typesafe" % "config" % ConfigVersion
+libraryDependencies += "com.typesafe.akka" %% "akka-testkit" % AkkaVersion % "test"
+libraryDependencies += "org.scalatest" %% "scalatest" % ScalaTestVer % "test"
--- src/main/scala/actors/OrderProcessor.scala
+++ src/main/scala/actors/OrderProcessor.scala
@@ -1,6 +1,7 @@
 package actors
 
-import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
+import akka.event.Logging
 import phones.{myPhone1, myPhone2, myPhone3}
 
 import scala.collection.parallel.CollectionConverters.{MapIsParallelizable, seqIsParallelizable}
@@ -10,32 +11,33 @@ object OrderProcessor {
   def props(phoneManufacturer: ActorRef): Props = Props(new OrderProcessor(phoneManufacturer))
 }
 
-class OrderProcessor(pManu: ActorRef) extends Actor {
+class OrderProcessor(pManu: ActorRef) extends Actor with ActorLogging{
   override def receive: Receive = {
     case order: Map[String, Int] =>
 //      println("Processing order")
       order.par.foreach(deliverable => (1 to deliverable._2 toList).par foreach(_ => pManu ! "Manufacture " + deliverable._1))
 
-      println("sent orders")
+      log.info("sent orders")
 
     case _: myPhone1 =>
-      println("Processed myPhone1")
+      log.info("Processed myPhone1")
 
     case _: myPhone2 =>
-      println("Processed myPhone2")
+      log.info("Processed myPhone2")
 
     case _: myPhone3 =>
-      println("Processed myPhone3")
+      log.info("Processed myPhone3")
   }
 }
 
 object Main extends App {
-  val order = Map[String, Int]("myPhone1" -> (Math.random() * 100).toInt, "myPhone2" -> (Math.random() * 100).toInt, "myPhone3" -> (Math.random() * 100).toInt)
-  println("Current order: " + order)
+  val order = Map[String, Int]("myPhone1" -> (Math.random() * 10).toInt, "myPhone2" -> (Math.random() * 10).toInt, "myPhone3" -> (Math.random() * 10).toInt)
   val system = ActorSystem("actorSystem")
+  private val log = Logging(system, getClass.getName)
+  log.info("Current order: " + order)
   val phoneManufacturer: ActorRef     = system.actorOf(Props[PhoneManufacturer])
   val orderProcessor: ActorRef = system.actorOf(OrderProcessor.props(phoneManufacturer))
-  println("Starting order")
+  log.info("Starting order")
   orderProcessor ! order
 }
 
