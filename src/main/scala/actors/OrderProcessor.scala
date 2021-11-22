bc3590193b4d8b52380d16f8a7e3261f739f29fd
my <my@MacBook-Air-2.local>
2021-11-22 12:13:50 -0600

    Added YAML snapshot functionality

--- src/main/scala/actors/OrderProcessor.scala
+++ src/main/scala/actors/OrderProcessor.scala
@@ -1,23 +1,30 @@
 package actors
 
-import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
+import actors.OrderProcessor.TakeSnapshot
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props, Timers}
 import akka.event.Logging
 import phones.{myPhone1, myPhone2, myPhone3}
 
 import scala.collection.parallel.CollectionConverters.{MapIsParallelizable, seqIsParallelizable}
+import scala.concurrent.duration.{Duration, FiniteDuration, MILLISECONDS}
 import scala.language.postfixOps
 
 object OrderProcessor {
   def props(phoneManufacturer: ActorRef): Props = Props(new OrderProcessor(phoneManufacturer))
+  case object TakeSnapshot
 }
 
-class OrderProcessor(pManu: ActorRef) extends Actor with ActorLogging{
+class OrderProcessor(pManu: ActorRef) extends Actor with ActorLogging with Timers{
+
+  timers.startTimerWithFixedDelay("capture-snapshot", TakeSnapshot, FiniteDuration(Duration("5 seconds").toMillis, MILLISECONDS))
+//  timers.startSingleTimer("capture", TakeSnapshot, FiniteDuration(Duration("5 seconds").toMillis, MILLISECONDS) )
   override def receive: Receive = {
     case order: Map[String, Int] =>
 //      println("Processing order")
       order.par.foreach(deliverable => (1 to deliverable._2 toList).par foreach(_ => pManu ! "Manufacture " + deliverable._1))
 
       log.info("sent orders")
+    case TakeSnapshot => pManu ! TakeSnapshot
 
     case _: myPhone1 =>
       log.info("Processed myPhone1")
@@ -31,7 +38,7 @@ class OrderProcessor(pManu: ActorRef) extends Actor with ActorLogging{
 }
 
 object Main extends App {
-  val order = Map[String, Int]("myPhone1" -> (Math.random() * 10).toInt, "myPhone2" -> (Math.random() * 10).toInt, "myPhone3" -> (Math.random() * 10).toInt)
+  val order = Map[String, Int]("myPhone1" -> (Math.random() * 5).toInt, "myPhone2" -> (Math.random() * 5).toInt, "myPhone3" -> (Math.random() * 5).toInt)
   val system = ActorSystem("actorSystem")
   private val log = Logging(system, getClass.getName)
   log.info("Current order: " + order)
--- src/main/scala/actors/PhoneManufacturer.scala
+++ src/main/scala/actors/PhoneManufacturer.scala
@@ -1,23 +1,63 @@
 package actors
 
-import akka.actor.{Actor, ActorRef, Props}
+import actors.OrderProcessor.TakeSnapshot
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import components.body.{B1, B2, B3}
 import components.camera.C1
 import components.processor.P1
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import phones.{myPhone1, myPhone2, myPhone3}
 
-class PhoneManufacturer extends Actor{
+import java.io.FileWriter
+import scala.beans.BeanProperty
+import scala.concurrent.duration.{Duration, FiniteDuration, MILLISECONDS}
+
+class PhoneManufacturer extends Actor with ActorLogging{
+  var Phone1Manufactured = 0
+  var Phone2Manufactured = 0
+  var Phone3Manufactured = 0
+  private val manufacturingTime = FiniteDuration(Duration("1 seconds").toMillis, MILLISECONDS)
+
+  case class PhoneManufacturerPersistence(@BeanProperty var Phone1Manufactured: Int, @BeanProperty var Phone2Manufactured: Int, @BeanProperty var Phone3Manufactured: Int)
+
+  def captureState()= {
+    val pm = PhoneManufacturerPersistence(Phone1Manufactured, Phone2Manufactured, Phone3Manufactured)
+    val writer = new FileWriter("sample.yaml")
+    val mapper = new ObjectMapper(new YAMLFactory())
+    println(pm)
+    mapper.writeValue(writer, pm)
+    println("Data persisted in yaml")
+    log.info(""+getClass.getName
+      +":\n - "+getClass.getDeclaredField("Phone1Manufactured").toString+" : "+Phone1Manufactured
+      +"\n - "+getClass.getDeclaredField("Phone2Manufactured").toString+" : "+Phone2Manufactured
+      +"\n - "+getClass.getDeclaredField("Phone3Manufactured").toString+" : "+Phone3Manufactured)
+  }
+
   override def receive: Receive = {
+    case TakeSnapshot => captureState()
     case "Manufacture myPhone1" =>
 //      println("Received order to process myPhone1")
+      val loopTil = System.nanoTime()+manufacturingTime.toNanos
+      while (System.nanoTime() < loopTil) {
+      }
+      Phone1Manufactured += 1
       sender() ! new myPhone1(new P1(), new C1, new B1)
 
     case "Manufacture myPhone2" =>
 //      println("Received order to process myPhone2")
+      val loopTil = System.nanoTime()+manufacturingTime.toNanos
+      while (System.nanoTime() < loopTil) {
+      }
+      Phone2Manufactured += 1
       sender() ! new myPhone2(new P1(), new C1, new B2)
 
     case "Manufacture myPhone3" =>
 //      println("Received order to process myPhone3")
+      val loopTil = System.nanoTime()+manufacturingTime.toNanos
+      while (System.nanoTime() < loopTil) {
+      }
+      Phone3Manufactured += 1
       sender() ! new myPhone3(new P1(), new C1, new B3)
 
   }
